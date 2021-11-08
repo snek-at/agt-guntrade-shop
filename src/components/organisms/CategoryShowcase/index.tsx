ea523ae87f3fd60ea51bd6ef254f2bc42b94be15
petute <daniel.petutschnigg@gmail.com>
2021-11-09 00:05:30 +0100

    chore: change hero background image, add animation

--- src/components/organisms/CategoryShowcase/index.tsx
+++ src/components/organisms/CategoryShowcase/index.tsx
@@ -21,69 +21,67 @@ const CategoryShowcase = ({tabs}: CategoryShowcaseProps) => {
   const categories = Object.keys(tabs)
 
   return (
-    <Center>
-      <Box w="65%">
-        <Flex>
-          {categories.map((category, index) => {
-            return (
-              <Box
-                _first={{borderTopLeftRadius: '3px'}}
-                _last={{borderTopRightRadius: '3px'}}
-                cursor="pointer"
-                bg={current === category ? 'white' : 'agt.gray'}
-                py="3"
-                px="5"
-                onClick={() => {
-                  setCurrent(category)
-                  setDirection(
-                    index > categories.indexOf(current) ? 'right' : 'left'
-                  )
-                }}>
-                <Text
-                  fontSize="14"
-                  fontWeight="bold"
-                  casing="uppercase"
-                  color={current === category ? 'black' : 'white'}>
-                  {category}
-                </Text>
-              </Box>
-            )
-          })}
-        </Flex>
-        <Box
-          h="300px"
-          w="1175px"
-          p="10"
-          pb="20"
-          bg="white"
-          borderBottomRadius="3px"
-          borderTopRightRadius="3px">
-          {categories.map(category => {
-            return (
-              <CategoryTab
-                visible={current === category ? 'visible' : 'hidden'}
-                items={tabs[category].items}
-                images={tabs[category].images}
-                direction={direction}
-              />
-            )
-          })}
-        </Box>
-        <Center>
-          <Button
-            borderRadius="3px"
-            bg="agt.gray"
-            color="white"
-            _hover={{backgroundColor: 'gray.700'}}
-            variant="solid"
-            size="lg"
-            mt="-6"
-            onClick={() => null}>
-            Mehr davon
-          </Button>
-        </Center>
+    <Box w="65%" mx="auto">
+      <Flex>
+        {categories.map((category, index) => {
+          return (
+            <Box
+              _first={{borderTopLeftRadius: '3px'}}
+              _last={{borderTopRightRadius: '3px'}}
+              cursor="pointer"
+              bg={current === category ? 'white' : 'agt.gray'}
+              py="3"
+              px="5"
+              onClick={() => {
+                setCurrent(category)
+                setDirection(
+                  index > categories.indexOf(current) ? 'right' : 'left'
+                )
+              }}>
+              <Text
+                fontSize="14"
+                fontWeight="bold"
+                casing="uppercase"
+                color={current === category ? 'black' : 'white'}>
+                {category}
+              </Text>
+            </Box>
+          )
+        })}
+      </Flex>
+      <Box
+        h="300px"
+        w="1175px"
+        p="10"
+        pb="20"
+        bg="white"
+        borderBottomRadius="3px"
+        borderTopRightRadius="3px">
+        {categories.map(category => {
+          return (
+            <CategoryTab
+              visible={current === category ? 'visible' : 'hidden'}
+              items={tabs[category].items}
+              images={tabs[category].images}
+              direction={direction}
+            />
+          )
+        })}
       </Box>
-    </Center>
+      <Center>
+        <Button
+          borderRadius="3px"
+          bg="agt.gray"
+          color="white"
+          _hover={{backgroundColor: 'gray.700'}}
+          variant="solid"
+          size="lg"
+          mt="-6"
+          onClick={() => null}>
+          Mehr davon
+        </Button>
+      </Center>
+    </Box>
   )
 }
 
--- src/components/organisms/sections/HeroSection/index.tsx
+++ src/components/organisms/sections/HeroSection/index.tsx
@@ -1,98 +1,114 @@
-import {Box, Flex} from '@chakra-ui/react'
+import {Box, Flex, Image, ImageProps, Center} from '@chakra-ui/react'
 
 import BulletShowcase from '../../BulletShowcase'
 import CategoryShowcase from '../../CategoryShowcase'
 import AccessorieShowcase from '../../AccessorieShowcase'
 import WeaponShowcase from '../../WeaponShowcase'
+import {motion} from 'framer-motion'
 
 export interface HeroSectionProps {}
 
 const HeroSection = () => {
-  return (
-    <Box backgroundImage="https://images4.alphacoders.com/968/thumb-1920-96814.jpg">
-      <Box p="10">
-        <WeaponShowcase
-          weapons={[
-            {
-              title: 'Peng',
-              description: 'This is a weapon',
-              image:
-                'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
-              price: 100
-            },
-            {
-              title: 'Bang',
-              description: 'This is a weapon',
-              image:
-                'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
-              price: 700
-            },
-            {
-              title: 'Bum',
-              description: 'This is a weapon',
-              image:
-                'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
-              price: 800
-            }
-          ]}
-        />
-        <CategoryShowcase
-          tabs={{
-            New: {
-              items: ['AR15', 'AR15', 'AR15', 'AR15', 'AR15', 'AR15'],
-              images: [
-                'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
-                'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
-                'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
-                'https://www.lwrci.com/assets/images/5563.jpg',
-                'https://media.istockphoto.com/photos/hunting-rifle-with-cartridge-picture-id186779464',
-                'https://www.taiwangun.com/img/product_media/85001-86000/karabiny-szturmowe-elektryczne-ar15-lite-cqb-at-ny02-cq-arcturus-85431(1).JPG'
-              ]
-            },
-            Handfeuerwaffen: {
-              items: [
-                'Pistolen',
-                'Pistolen',
+  const MotionImage = motion<ImageProps>(Image)
 
-                'Pistolen',
-                'Pistolen',
-                'Pistolen',
-                'Pistolen'
-              ],
-              images: [
-                'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
-                'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
-                'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
-                'https://www.lwrci.com/assets/images/5563.jpg',
-                'https://media.istockphoto.com/photos/hunting-rifle-with-cartridge-picture-id186779464',
-                'https://www.taiwangun.com/img/product_media/85001-86000/karabiny-szturmowe-elektryczne-ar15-lite-cqb-at-ny02-cq-arcturus-85431(1).JPG'
-              ]
-            },
-            Flinten: {
-              items: [
-                'Flinten',
-                'Flinten',
-                'Flinten',
-                'Flinten',
-                'Flinten',
-                'Flinten'
-              ],
-              images: [
-                'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
-                'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
-                'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
-                'https://www.lwrci.com/assets/images/5563.jpg',
-                'https://media.istockphoto.com/photos/hunting-rifle-with-cartridge-picture-id186779464',
-                'https://www.taiwangun.com/img/product_media/85001-86000/karabiny-szturmowe-elektryczne-ar15-lite-cqb-at-ny02-cq-arcturus-85431(1).JPG'
-              ]
-            }
-          }}
-        />
-        <Flex mt="8" justifyContent="center" alignContent="center" mx="auto">
-          <BulletShowcase />
-          <AccessorieShowcase />
-        </Flex>
-      </Box>
+  return (
+    <Box w="100%" position="relative" overflow="hidden">
+      <MotionImage
+        src="https://images4.alphacoders.com/968/thumb-1920-96814.jpg"
+        alt="background"
+        position="absolute"
+        top="0"
+        left="0"
+        zIndex="-1"
+        h="auto"
+        animate={{scale: 1.1}}
+        transition={{
+          ease: 'linear',
+          duration: 10,
+          repeat: Infinity
+        }}
+      />
+      <WeaponShowcase
+        weapons={[
+          {
+            title: 'Peng',
+            description: 'This is a weapon',
+            image:
+              'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
+            price: 100
+          },
+          {
+            title: 'Bang',
+            description: 'This is a weapon',
+            image:
+              'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
+            price: 700
+          },
+          {
+            title: 'Bum',
+            description: 'This is a weapon',
+            image:
+              'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
+            price: 800
+          }
+        ]}
+      />
+      <CategoryShowcase
+        tabs={{
+          New: {
+            items: ['AR15', 'AR15', 'AR15', 'AR15', 'AR15', 'AR15'],
+            images: [
+              'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
+              'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
+              'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
+              'https://www.lwrci.com/assets/images/5563.jpg',
+              'https://media.istockphoto.com/photos/hunting-rifle-with-cartridge-picture-id186779464',
+              'https://www.taiwangun.com/img/product_media/85001-86000/karabiny-szturmowe-elektryczne-ar15-lite-cqb-at-ny02-cq-arcturus-85431(1).JPG'
+            ]
+          },
+          Handfeuerwaffen: {
+            items: [
+              'Pistolen',
+              'Pistolen',
+
+              'Pistolen',
+              'Pistolen',
+              'Pistolen',
+              'Pistolen'
+            ],
+            images: [
+              'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
+              'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
+              'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
+              'https://www.lwrci.com/assets/images/5563.jpg',
+              'https://media.istockphoto.com/photos/hunting-rifle-with-cartridge-picture-id186779464',
+              'https://www.taiwangun.com/img/product_media/85001-86000/karabiny-szturmowe-elektryczne-ar15-lite-cqb-at-ny02-cq-arcturus-85431(1).JPG'
+            ]
+          },
+          Flinten: {
+            items: [
+              'Flinten',
+              'Flinten',
+              'Flinten',
+              'Flinten',
+              'Flinten',
+              'Flinten'
+            ],
+            images: [
+              'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
+              'https://www.fieldandstream.com/uploads/2019/01/18/7TUMLR2JZRP7F52FH3ENRNVQ6A.jpg',
+              'https://upload.wikimedia.org/wikipedia/commons/8/8f/Webley_IMG_6789.jpg',
+              'https://www.lwrci.com/assets/images/5563.jpg',
+              'https://media.istockphoto.com/photos/hunting-rifle-with-cartridge-picture-id186779464',
+              'https://www.taiwangun.com/img/product_media/85001-86000/karabiny-szturmowe-elektryczne-ar15-lite-cqb-at-ny02-cq-arcturus-85431(1).JPG'
+            ]
+          }
+        }}
+      />
+      <Flex mt="8" w="65%" mx="auto">
+        <BulletShowcase />
+        <AccessorieShowcase />
+      </Flex>
     </Box>
   )
 }
