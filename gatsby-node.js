5dad09f284c4c06cac64cbe7489478a3bf7ddd0c
petute <daniel.petutschnigg@gmail.com>
2021-12-03 00:20:32 +0100

    feat: fetch google reviews

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,83 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const axios = require('axios')
+const dotenv = require('dotenv')
 
-// You can delete this file if you're not using it
+exports.onPreInit = () => console.log('Loading google reviews')
+
+const NODE_TYPE = 'GoogleReview'
+
+exports.sourceNodes = async ({actions, createNodeId, createContentDigest}) => {
+  const {createNode} = actions
+
+  dotenv.config()
+
+  const apiKey = process.env.APIKEY
+  const placeId = process.env.PLACE_ID
+
+  if (!apiKey || typeof apiKey !== 'string') {
+    throw new Error(
+      'You must supply a valid API Key from Scale Serp. Visit https://scaleserp.com/ for more information.'
+    )
+  }
+
+  if (!placeId || typeof placeId !== 'string') {
+    throw new Error(
+      'You must supply a valid place id from Google. You can find your place id at https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder.'
+    )
+  }
+
+  const params = {
+    api_key: apiKey,
+    search_type: 'place_reviews',
+    place_id: placeId,
+    hl: 'de',
+    gl: 'at'
+  }
+
+  const response = await axios
+    .get('https://api.scaleserp.com/search', {
+      params
+    })
+    .catch(error => {
+      throw new Error(`Error fetching results from ScaleSerp API: ${error}`)
+    })
+
+  if (!response || !response.data)
+    throw new Error(`Error fetching results from ScaleSerp API:`)
+
+  const reviews = response.data.place_reviews_results || []
+  reviews.forEach((review, i) => {
+    createNode({
+      ...review,
+      sourceImage: review.source_image,
+      sourceLink: review.source_link,
+      id: createNodeId(`${NODE_TYPE}-${i}`),
+      test: true,
+      parent: null,
+      children: [],
+      internal: {
+        type: NODE_TYPE,
+        content: JSON.stringify(review),
+        contentDigest: createContentDigest(review)
+      }
+    })
+  })
+}
+
+exports.createSchemaCustomization = ({actions}) => {
+  const {createTypes} = actions
+  const typeDefs = `
+    type GoogleReview implements Node @dontInfer {
+      rating: Int
+      position: Int
+      body: String
+      date: String
+      sourceLink: String
+      sourceImage: String
+      position: Int
+      source: String
+      position: Int
+    }
+  `
+
+  createTypes(typeDefs)
+}
--- src/components/organisms/sections/ReviewSection/index.tsx
+++ src/components/organisms/sections/ReviewSection/index.tsx
@@ -1,57 +1,9 @@
-import {Box, Heading, Text, Wrap} from '@chakra-ui/layout'
-import React from 'react'
-import ReviewCard from '../../../molecules/ReviewCard'
+import {Box} from '@chakra-ui/layout'
 
-export interface ReviewSectionProps {
-  slogan: React.ReactNode
-  review1text: React.ReactNode
-  review1bio: React.ReactNode
-  review1image: React.ReactNode
-  review2text: React.ReactNode
-  review2bio: React.ReactNode
-  review2image: React.ReactNode
-  review3text: React.ReactNode
-  review3bio: React.ReactNode
-  review3image: React.ReactNode
-}
+export interface ReviewSectionProps {}
 
-const ReviewSection = ({
-  slogan,
-  review1bio,
-  review1image,
-  review1text,
-  review2bio,
-  review2image,
-  review2text,
-  review3bio,
-  review3image,
-  review3text
-}: ReviewSectionProps) => {
-  return (
-    <Box>
-      <Box textAlign="center" mt="10">
-        <Heading mb="5">Customer Reviews</Heading>
-        <Text fontSize="20">{slogan}</Text>
-        <Wrap mt="10" justify="center" spacing="10">
-          <ReviewCard
-            reviewtext={review1text}
-            reviewbio={review1bio}
-            reviewimage={review1image}
-          />
-          <ReviewCard
-            reviewtext={review2text}
-            reviewbio={review2bio}
-            reviewimage={review2image}
-          />
-          <ReviewCard
-            reviewtext={review3text}
-            reviewbio={review3bio}
-            reviewimage={review3image}
-          />
-        </Wrap>
-      </Box>
-    </Box>
-  )
+const ReviewSection = () => {
+  return <Box></Box>
 }
 
 export default ReviewSection
