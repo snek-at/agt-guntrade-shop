0f997a959d27657ea996e56194c2e76b5033935c
petute <daniel.petutschnigg@gmail.com>
2021-12-12 18:43:23 +0100

    chore: build index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import NewsSection from '../components/organisms/sections/NewsSection'
 import HeroSection from '../components/organisms/sections/HeroSection'
 import ReviewSection from '../components/organisms/sections/ReviewSection'
 import FeaturedProductsSection from '../components/organisms/sections/FeaturedProductsSection/2'
+import AboutSection from '../components/organisms/sections/AboutSection'
 //#endregion
 
 //#region > Functions
@@ -123,6 +124,29 @@ const IndexPage = ({data}) => {
           />
         }
         data={data.allGoogleReview.nodes}
+      />
+      <AboutSection
+        heading={
+          <fields.TextField
+            fieldName="about-heading"
+            initValue="<p>I'm a heading</p>"
+            rtf={false}
+          />
+        }
+        teaser={
+          <fields.TextField
+            fieldName="about-teaser"
+            initValue="<p>I'm a teaser</p>"
+            rtf={false}
+          />
+        }
+        text={
+          <fields.TextField
+            fieldName="about-text"
+            initValue="<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>"
+            rtf={false}
+          />
+        }
       />
     </Box>
   )
