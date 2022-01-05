4649916a2dcfd29c200d492b2ca29bb822e01448
Daniel Petutschnigg <daniel.petutschnigg@gmail.com>
2022-01-05 09:57:17 +0100

    chore: add breadcrumb

---
+++ src/components/molecules/Breadcrumb/index.tsx
@@ -0,0 +1,53 @@
+import {RiHomeLine} from '@react-icons/all-files/ri/RiHomeLine'
+import {ChevronRightIcon} from '@chakra-ui/icons'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Icon
+} from '@chakra-ui/react'
+
+import {SeperatorStyle} from './style'
+
+interface BreadcrumbPops {
+  breadcrumb: string
+}
+
+const Export = ({breadcrumb}: BreadcrumbPops) => (
+  <Breadcrumb
+    pt="1"
+    borderRadius="5px"
+    color="white"
+    px="2"
+    w="fit-content"
+    bg="agt.gray"
+    justifyContent="center"
+    alignItems="center"
+    separator={<ChevronRightIcon boxSize="2rem" className="icon" />}>
+    <BreadcrumbItem position="relative" css={SeperatorStyle}>
+      <BreadcrumbLink>
+        <Icon
+          as={RiHomeLine}
+          boxSize="1.25rem"
+          mb="1.5"
+          _hover={{color: 'agt.red'}}
+          transition="0.3s"
+        />
+      </BreadcrumbLink>
+    </BreadcrumbItem>
+    {breadcrumb.split('/').map(crumb => (
+      <BreadcrumbItem>
+        <BreadcrumbLink
+          fontSize="1.25rem"
+          to=""
+          _hover={{color: 'agt.red'}}
+          transition="0.3s"
+          fontWeight="100">
+          {crumb}
+        </BreadcrumbLink>
+      </BreadcrumbItem>
+    ))}
+  </Breadcrumb>
+)
+
+export default Export
