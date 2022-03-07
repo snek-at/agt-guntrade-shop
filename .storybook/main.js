d2faffac1f6602423ba07e565425b79ef3eb9db2
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-03-07 19:55:12 +0000

    add workarounds to enable storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,36 @@ module.exports = {
     '@storybook/addon-essentials',
     'storybook-addon-gatsby'
   ],
+  webpackFinal: config => {
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    config.module.rules[0].exclude = [/core-js/]
+
+    // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+
+    config.resolve.mainFields = ['browser', 'module', 'main']
+
+    console.log(path.resolve(__dirname, '../node_modules/@jaenjs/jaen/src'))
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@jaen': path.resolve(__dirname, '../node_modules/@jaenjs/jaen/src'),
+      '@jaen-admin': path.resolve(
+        __dirname,
+        '../node_modules/@jaenjs/jaen/src/internal-plugins/admin'
+      ),
+      '@jaen-pages': path.resolve(
+        __dirname,
+        '../node_modules/@jaenjs/jaen/src/internal-plugins/pages'
+      ),
+      '@jaen-notify': path.resolve(
+        __dirname,
+        '../node_modules/@jaenjs/jaen/src/internal-plugins/notify'
+      )
+    }
+
+    return config
+  },
   framework: '@storybook/react',
   core: {builder: 'webpack5'}
 }
