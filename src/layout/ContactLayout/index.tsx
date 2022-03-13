f68aaab1e762ee44ab3a3e5838a9811a39019984
Florian Kleber <kleberbaum@erebos.xyz>
2022-03-13 05:25:12 +0000

    chore: add basic imprint page
     - Add contact layout
     - Add imrpint layout
     - Add legal layout
     - Add terms layout
     - Add imprint story
     - Add imprint page

---
+++ src/layout/ContactLayout/index.tsx
@@ -0,0 +1,56 @@
+import {
+  Box,
+  Button,
+  CloseButton,
+  Flex,
+  Heading,
+  Container,
+  HStack,
+  Icon,
+  Link,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Text,
+  useColorModeValue,
+  VStack
+} from '@chakra-ui/react'
+import {BaseLayout} from '../BaseLayout'
+import React, {useEffect, useState} from 'react'
+import {CheckCircleIcon, EmailIcon, NotAllowedIcon} from '@chakra-ui/icons'
+import {CookieModalService, useCookieState} from '../../services/cookiemodal'
+
+export interface ContactLayoutProps {
+  heading: React.ReactNode
+  city: React.ReactNode
+  zip_code: React.ReactNode
+  address: React.ReactNode
+  telephone: React.ReactNode
+  telefax: React.ReactNode
+  whatsapp_telephone: React.ReactNode
+  whatsapp_contactline: React.ReactNode
+  email: React.ReactNode
+  copyrightholder: React.ReactNode
+}
+
+export const ContactLayout = (props: ContactLayoutProps) => {
+
+  return (
+    <>
+      <Heading as="h4" size="md" mt="4">
+        {props.heading}
+      </Heading>
+      {props.city}
+      {props.zip_code}
+      {props.address}
+      {props.telephone}
+      {props.telefax}
+      {props.whatsapp_telephone}
+      {props.whatsapp_contactline}
+      {props.email}
+      {props.copyrightholder}
+    </>
+  )
+}
