27acf8705175b5c4be948f02fe89b07282c6c73d
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-03-15 20:07:48 +0000

    fix: make mapbox token available in browser ref: https://www.gatsbyjs.com/docs/how-to/local-development/environment-variables/

--- src/layout/MapLayout/index.tsx
+++ src/layout/MapLayout/index.tsx
@@ -1,10 +1,9 @@
-
-import React, { useRef, useEffect, useState } from 'react';
+import React, {useRef, useEffect, useState} from 'react'
 import {Box} from '@chakra-ui/react'
-import mapboxgl from '!mapbox-gl';
+import mapboxgl from '!mapbox-gl'
 import {MapStyle} from './style'
 
-mapboxgl.accessToken = process.env.MAPBOX
+mapboxgl.accessToken = process.env.GATSBY_MAPBOX_ACCESS_TOKEN
 
 export interface MapLayoutProps {
   lng: number
@@ -13,38 +12,35 @@ export interface MapLayoutProps {
 }
 
 export const MapLayout = (props: MapLayoutProps) => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(props.lng);
-  const [lat, setLat] = useState(props.lat);
-  const [zoom, setZoom] = useState(props.zoom);
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(props.lng)
+  const [lat, setLat] = useState(props.lat)
+  const [zoom, setZoom] = useState(props.zoom)
 
   useEffect(() => {
     // initialize map only once
-    if (map.current) return;
+    if (map.current) return
 
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom
-    });
-  });
+    })
+  })
 
   useEffect(() => {
-    if (!map.current){
+    if (!map.current) {
       return
-
-    }else 
-    {
+    } else {
       map.current.on('move', () => {
-        setLng(map.current.getCenter().lng.toFixed(4));
-        setLat(map.current.getCenter().lat.toFixed(4));
-        setZoom(map.current.getZoom().toFixed(2));
-      });
+        setLng(map.current.getCenter().lng.toFixed(4))
+        setLat(map.current.getCenter().lat.toFixed(4))
+        setZoom(map.current.getZoom().toFixed(2))
+      })
     }
-
-  });
+  })
 
   return (
     <div>
@@ -53,5 +49,5 @@ export const MapLayout = (props: MapLayoutProps) => {
       </div>
       <Box ref={mapContainer} css={MapStyle()} className="map-container" />
     </div>
-  );
+  )
 }
