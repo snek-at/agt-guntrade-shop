85aa8dbfe834bb0bdafe5eb8f037592caf50e7b2
Florian Kleber <kleberbaum@erebos.xyz>
2022-03-17 18:31:02 +0000

    chore: update map
     - change map style
     - update data handling

--- src/components/organisms/Map/Map.tsx
+++ src/components/organisms/Map/Map.tsx
@@ -1,66 +1,146 @@
-import * as React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useMemo} from 'react'
-import Map, {
-  Marker,
-  NavigationControl,
-  FullscreenControl,
-  ScaleControl,
-  GeolocateControl
+import ReactMapGL, {
+    Popup,
+    Marker,
+    NavigationControl,
+    FullscreenControl,
+    ScaleControl,
+    GeolocateControl,
+    ViewState
 } from 'react-map-gl'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
-
 import Pin from './pin'
-import {Box} from '@chakra-ui/react'
+import {Box, Heading, Text} from '@chakra-ui/react'
 
-const CITIES = [
-  {
-    city: 'New York',
-    population: '8,175,133',
-    image:
-      'http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg',
-    state: 'New York',
-    latitude: 40.6643,
-    longitude: -73.9385
-  }
-]
+export interface IMarker {
+    name: string
+    city: string
+    zip_code: string
+    address: string
+    image: React.ReactNode
+    description: string
+    latitude: number
+    longitude: number
+    handleClick?(): void
+    handleMouseEnter?(e: React.MouseEvent): void
+    handleMouseLeave?(e: React.MouseEvent): void
+}
+
+export interface IMap {
+    width?: number
+    height?: number
+    latitude: number
+    longitude: number
+    zoom?: number
+    layerColour?: string
+    markers: IMarker[]
+    buttons?: JSX.Element[]
+}
+
 
 const TOKEN = process.env.GATSBY_MAPBOX_ACCESS_TOKEN
 
-export default function App() {
-  const pins = useMemo(
-    () =>
-      CITIES.map((city, index) => (
-        <Marker
-          key={`marker-${index}`}
-          longitude={city.longitude}
-          latitude={city.latitude}
-          anchor="bottom">
-          <Pin onClick={() => null} />
-        </Marker>
-      )),
-    []
-  )
+const Map = (props: IMap):JSX.Element => {
+    const [popupInfo, setPopupInfo] = useState<IMarker|undefined>(undefined);
 
-  return (
-    <Box w="100vw" bg="red">
-      <Map
-        initialViewState={{
-          latitude: 40,
-          longitude: -100,
-          zoom: 3.5,
-          bearing: 0,
-          pitch: 0
-        }}
-        style={{width: '100%', height: 400}}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken={TOKEN}>
-        <GeolocateControl position="top-left" />
-        <FullscreenControl position="top-left" />
-        <NavigationControl position="top-left" />
-        <ScaleControl />
-        {pins}
-      </Map>
-    </Box>
-  )
+    const viewport: ViewState = {
+        latitude: props.latitude ? props.latitude : 40,
+        longitude: props.longitude ? props.longitude : -100,
+        zoom: (typeof props.zoom !== "undefined") ? props.zoom : 10,
+        bearing: 0,
+        pitch: 0,
+        padding: {
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right:0
+        }
+    }
+
+    const markes = useMemo(
+        () =>
+            props.markers.map((marker, index) => {
+
+                const handleClick = (marker: any) => {
+                    if (typeof marker.handleClick === "function") {
+                        marker.handleClick();
+                    } else {
+                        setPopupInfo(marker)
+                    }
+                    
+                }
+            
+                const handleMouseEnter = (e: React.MouseEvent) => {
+                    if (typeof marker.handleMouseEnter === "function") {
+                        marker.handleMouseEnter(e);
+                    }
+                }
+            
+                const handleMouseLeave = (e: React.MouseEvent) => {
+                    if (typeof marker.handleMouseLeave === "function") {
+                        marker.handleMouseLeave(e);
+                    }
+                }
+                
+                return(
+                <div
+                    onMouseEnter={(e) => handleMouseEnter(e)}
+                    onMouseLeave={(e) => handleMouseLeave(e)}>
+                    <Marker
+                        key={`marker-${index}`}
+                        longitude={marker.longitude}
+                        latitude={marker.latitude}
+                        anchor="bottom">
+                        <Pin onClick={() => handleClick(marker)} />
+                    </Marker>
+                </div>
+            )
+            }),
+        []
+    )
+
+    return (
+        <Box>
+        <ReactMapGL
+            initialViewState={viewport}
+            style={{width: '100%', height: 400}}
+            mapStyle="mapbox://styles/kleberbaum/cl0v92bz0000j14o5nejnswcn"
+            mapboxAccessToken={TOKEN}>
+            <GeolocateControl position="top-left" />
+            <FullscreenControl position="top-left" />
+            <NavigationControl position="top-left" />
+            <ScaleControl />
+            {markes}
+            {popupInfo && (
+            <Popup
+                anchor="top"
+                longitude={Number(popupInfo.longitude)}
+                latitude={Number(popupInfo.latitude)}
+                closeOnClick={false}
+                onClose={() => setPopupInfo(undefined)}
+            >
+                <Box>
+                    <Heading as="h4" size="sm" mb="1">
+                        {popupInfo.name}
+                    </Heading>
+                    <Text>
+                        {popupInfo.zip_code}, {popupInfo.city}
+                    </Text>
+                    <Text>
+                        {popupInfo.address}
+                    </Text>
+                    <Text>
+                        {popupInfo.description}
+                    </Text>
+                </Box>
+                {popupInfo.image}
+            </Popup>
+            )}
+        </ReactMapGL>
+        </Box>
+    )
 }
+
+export default Map
--- src/layout/MapLayout/index.tsx
+++ src/layout/MapLayout/index.tsx
@@ -3,6 +3,7 @@ import {Flex, Box, BoxProps} from '@chakra-ui/react'
 import mapboxgl from 'mapbox-gl'
 import {MapStyle} from './style'
 import Map from '../../components/organisms/Map/Map'
+import {Logo} from '../../common/assets'
 
 
 mapboxgl.accessToken = process.env.GATSBY_MAPBOX_ACCESS_TOKEN!
@@ -17,10 +18,25 @@ interface IProps extends BoxProps {
   interactive?: boolean
 }
 
+// <StaticImage style={{width: '100%'}} src={popupInfo.image} alt={popupInfo.name} />
+// <GatsbyImage image={gatsbyimage.gatsbyImageData} alt={popupInfo.name} />
+const markers = [
+  {
+    name: 'AGT Guntrade',
+    city: 'Ried im Innkreis',
+    zip_code:'4910',
+    address: 'Wildfellnerstraße 22/1',
+    image: <Logo />,
+    description: '',
+    latitude: 46.53825,
+    longitude: 14.29214
+  }
+]
+
 export const MapLayout = ({ location, title, subtitle, className, googleMapsLink, startZoom = 10, endZoom = 11, interactive = false, ...props }: IProps) => {
   return (
     // <div className={styles.container}>
-      <Map lat={-27} lng={153} />
+      <Map latitude={46.53825} longitude={14.29214} zoom={16} markers={markers} />
     // </div>
   )
 }
